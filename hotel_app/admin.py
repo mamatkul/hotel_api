e4e088584a0d680f74c483c3c73662a4783ee9a4
Mamatkul <mamatqul.1997@gmail.com>
2023-04-13 17:09:14 +0300

    First commit

--- hotel_app/admin.py
+++ hotel_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Guest, Hotel, Room, Booking
 
-# Register your models here.
+admin.site.register(Guest)
+admin.site.register(Hotel)
+admin.site.register(Room)
+admin.site.register(Booking)
